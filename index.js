31a9668f182344727afc7ebcd4c103d766f27695
Jared Allard <jaredallard@users.noreply.github.com>
2023-12-03 16:07:20 -0800

    obligatory keep this mess running
    Updates to latest `yarn`, removes Twitter (I don't use it anymore) and
    updates all dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * Turn your Now Playing into your Twitter Status~
+ * Turns your now playing into a cool website. Yeah. That's all it does.
  *
  * @author Jared Allard <jaredallard@outlook.com>
  * @version 1
@@ -7,15 +7,13 @@
  */
 
 const debug = require('debug')('musictweet')
-const Twit = require('twit')
 const events = require('events')
 
 const config = require('./config/config.json')
 
-// modules / Twitter
+// modules
 const source = require('./sources/spotify.js')
 const web = require('./lib/web.js')
-const T = new Twit(config.twitter)
 
 const status = new events.EventEmitter
 
@@ -23,40 +21,14 @@ process.on('unhandledRejection', reason => {
   console.log('Unhandled Promise Rejection', reason)
 });
 
-status.on('changed', async song => {
-  let formatted = config.template
-    .replace('{{song}}', song.name)
-    .replace('{{artist}}', song.artist)
-    .replace('{{url}}', song.url)
-
-  const update = {
-    skip_status: 1,
-    name: formatted
-  }
-
-  if (config.bio_template) {
-    const bio = config.bio_template
-      .replace('{{song}}', song.name)
-      .replace('{{artist}}', song.artist)
-      .replace('{{url}}', song.url)
-
-    update.description = bio
-  }
-
-  if (formatted.length > 50) {
-    return console.error("name is too long :(")
-  }
-
-  await T.post('account/update_profile', update)
-})
-
 status.on('changed', song => {
   console.log('new song', `${song.name} by ${song.artist}`)
 })
 
 const init = async () => {
   const provider = await source(config, status)
-  console.log(provider)
+
+  console.log("api started on :8080")
   web(provider, status)
 }
 
